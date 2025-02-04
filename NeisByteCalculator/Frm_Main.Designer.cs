7c1d0073715aca0e40efcb85c4e0659e6b13a210
Howard Moon <howard@ubivelox.com>
2025-02-04 11:05:47 +0900

    기본 화면 구성
    기본 화면 구조 만들기, 아직 방향 테스트 중

---
+++ NeisByteCalculator/Frm_Main.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace NeisByteCalculator
+{
+    partial class Frm_Main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.lbl_BytesInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lbl_BytesInfo);
+            this.splitContainer1.Panel2.Controls.Add(this.label11);
+            this.splitContainer1.Panel2.Controls.Add(this.label12);
+            this.splitContainer1.Panel2.Controls.Add(this.label9);
+            this.splitContainer1.Panel2.Controls.Add(this.label10);
+            this.splitContainer1.Panel2.Controls.Add(this.label7);
+            this.splitContainer1.Panel2.Controls.Add(this.label8);
+            this.splitContainer1.Panel2.Controls.Add(this.label5);
+            this.splitContainer1.Panel2.Controls.Add(this.label6);
+            this.splitContainer1.Panel2.Controls.Add(this.label3);
+            this.splitContainer1.Panel2.Controls.Add(this.label4);
+            this.splitContainer1.Panel2.Controls.Add(this.label2);
+            this.splitContainer1.Panel2.Controls.Add(this.label1);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 425);
+            this.splitContainer1.SplitterDistance = 325;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "항목";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "바이트";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(128, 50);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 37);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "1,500 Bytes";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(128, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 37);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "자율활동";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(244, 50);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 37);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "1,500 Bytes";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(244, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 37);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "동아리활동";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(360, 50);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 37);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "2,100 Bytes";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(360, 12);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(110, 37);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "진로활동";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(476, 50);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(110, 37);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "1,500 Bytes";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(476, 12);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(110, 37);
+            this.label10.TabIndex = 8;
+            this.label10.Text = "교과세특";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(592, 50);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(110, 37);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "1.500 Bytes";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(592, 12);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(110, 37);
+            this.label12.TabIndex = 10;
+            this.label12.Text = "개인별세특";
+            this.label12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(157, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(429, 171);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // lbl_BytesInfo
+            // 
+            this.lbl_BytesInfo.Location = new System.Drawing.Point(690, 30);
+            this.lbl_BytesInfo.Name = "lbl_BytesInfo";
+            this.lbl_BytesInfo.Size = new System.Drawing.Size(110, 37);
+            this.lbl_BytesInfo.TabIndex = 12;
+            this.lbl_BytesInfo.Text = "1.500 Bytes";
+            this.lbl_BytesInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Frm_Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Frm_Main_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_BytesInfo;
+    }
+}
+
---
+++ NeisByteCalculator/Frm_Main.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeisByteCalculator
+{
+
+
+    public partial class Frm_Main : Form
+    {
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        public static extern int strlen(string str);
+
+        private Thread thread;
+
+        public Frm_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Main_Load(object sender, EventArgs e)
+        {
+            thread = new Thread(CalculateThread);
+            thread.IsBackground = true;
+            thread.Start();
+        }
+
+        private void CalculateThread()
+        {
+            try
+            {
+                while (true)
+                {
+
+                    Invoke(new Action(() =>
+                    {
+                        string input = richTextBox1.Text;
+                        lbl_BytesInfo.Text = CalculateByteSize(input).ToString("#,##0") + " bytes";
+
+
+                    }));
+
+
+                    Thread.Sleep(1);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+
+        public static int CountCharacter(string input, char targetChar)
+        {
+            int count = 0;
+            foreach (char c in input)
+            {
+                if (c == targetChar)
+                    count++;
+            }
+            return count;
+        }
+        public static int CalculateByteSize(string input)
+        {
+            int bytes = Encoding.UTF8.GetBytes(input).Length;
+
+            return bytes;// + CountCharacter(input, '\n');
+        }
+
+
+
+
+
+    }
+}
