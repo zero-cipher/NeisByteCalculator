6003b3bf272ccc8e7c04b4e769e7159e3365c90e
Howard Moon <howard@ubivelox.com>
2025-02-11 18:37:29 +0900

    폰트 관련 업데이트
    폰트, 폰트 크기 변경 기능 업데이트

--- NeisByteCalculator/Frm_Main.Designer.cs
+++ NeisByteCalculator/Frm_Main.Designer.cs
@@ -29,7 +29,14 @@ namespace NeisByteCalculator
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_Main));
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.cmd_Open = new System.Windows.Forms.ToolStripButton();
+            this.cmd_Save = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.cbo_FontName = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.cmd_Exit = new System.Windows.Forms.ToolStripButton();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.lbl_BytesInfo = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
@@ -44,7 +51,9 @@ namespace NeisByteCalculator
             this.label4 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
-            this.customRichTextBox1 = new NeisByteCalculator.CustomRichTextBox();
+            this.cbo_FontSize = new System.Windows.Forms.ToolStripComboBox();
+            this.txt_Input = new NeisByteCalculator.CustomRichTextBox();
+            this.toolStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
@@ -53,23 +62,79 @@ namespace NeisByteCalculator
             // 
             // toolStrip1
             // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cmd_Open,
+            this.cmd_Save,
+            this.toolStripSeparator1,
+            this.cbo_FontName,
+            this.cbo_FontSize,
+            this.toolStripSeparator2,
+            this.cmd_Exit});
             this.toolStrip1.Location = new System.Drawing.Point(0, 0);
             this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.Size = new System.Drawing.Size(800, 39);
             this.toolStrip1.TabIndex = 0;
             this.toolStrip1.Text = "toolStrip1";
+            // 
+            // cmd_Open
+            // 
+            this.cmd_Open.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmd_Open.Image = ((System.Drawing.Image)(resources.GetObject("cmd_Open.Image")));
+            this.cmd_Open.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.cmd_Open.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmd_Open.Name = "cmd_Open";
+            this.cmd_Open.Size = new System.Drawing.Size(36, 36);
+            this.cmd_Open.Text = "toolStripButton1";
+            this.cmd_Open.Click += new System.EventHandler(this.cmd_Open_Click);
+            // 
+            // cmd_Save
+            // 
+            this.cmd_Save.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmd_Save.Image = ((System.Drawing.Image)(resources.GetObject("cmd_Save.Image")));
+            this.cmd_Save.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.cmd_Save.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmd_Save.Name = "cmd_Save";
+            this.cmd_Save.Size = new System.Drawing.Size(36, 36);
+            this.cmd_Save.Text = "toolStripButton2";
+            this.cmd_Save.Click += new System.EventHandler(this.cmd_Save_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 39);
+            // 
+            // cbo_FontName
+            // 
+            this.cbo_FontName.Name = "cbo_FontName";
+            this.cbo_FontName.Size = new System.Drawing.Size(121, 39);
+            this.cbo_FontName.SelectedIndexChanged += new System.EventHandler(this.cbo_FontName_SelectedIndexChanged);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 39);
+            // 
+            // cmd_Exit
+            // 
+            this.cmd_Exit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.cmd_Exit.Image = ((System.Drawing.Image)(resources.GetObject("cmd_Exit.Image")));
+            this.cmd_Exit.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.cmd_Exit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.cmd_Exit.Name = "cmd_Exit";
+            this.cmd_Exit.Size = new System.Drawing.Size(36, 36);
+            this.cmd_Exit.Text = "toolStripButton9";
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 25);
+            this.splitContainer1.Location = new System.Drawing.Point(0, 39);
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.customRichTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.txt_Input);
             // 
             // splitContainer1.Panel2
             // 
@@ -86,15 +151,16 @@ namespace NeisByteCalculator
             this.splitContainer1.Panel2.Controls.Add(this.label4);
             this.splitContainer1.Panel2.Controls.Add(this.label2);
             this.splitContainer1.Panel2.Controls.Add(this.label1);
-            this.splitContainer1.Size = new System.Drawing.Size(800, 425);
-            this.splitContainer1.SplitterDistance = 307;
+            this.splitContainer1.Size = new System.Drawing.Size(800, 411);
+            this.splitContainer1.SplitterDistance = 300;
             this.splitContainer1.TabIndex = 1;
             // 
             // lbl_BytesInfo
             // 
-            this.lbl_BytesInfo.Location = new System.Drawing.Point(12, 76);
+            this.lbl_BytesInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_BytesInfo.Location = new System.Drawing.Point(12, 67);
             this.lbl_BytesInfo.Name = "lbl_BytesInfo";
-            this.lbl_BytesInfo.Size = new System.Drawing.Size(776, 29);
+            this.lbl_BytesInfo.Size = new System.Drawing.Size(670, 29);
             this.lbl_BytesInfo.TabIndex = 12;
             this.lbl_BytesInfo.Text = "1.500 Bytes";
             this.lbl_BytesInfo.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -102,7 +168,7 @@ namespace NeisByteCalculator
             // label11
             // 
             this.label11.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label11.Location = new System.Drawing.Point(592, 42);
+            this.label11.Location = new System.Drawing.Point(572, 39);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(110, 25);
             this.label11.TabIndex = 11;
@@ -114,7 +180,7 @@ namespace NeisByteCalculator
             this.label12.BackColor = System.Drawing.Color.Black;
             this.label12.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label12.ForeColor = System.Drawing.Color.White;
-            this.label12.Location = new System.Drawing.Point(592, 12);
+            this.label12.Location = new System.Drawing.Point(572, 12);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(110, 25);
             this.label12.TabIndex = 10;
@@ -124,7 +190,7 @@ namespace NeisByteCalculator
             // label9
             // 
             this.label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label9.Location = new System.Drawing.Point(476, 42);
+            this.label9.Location = new System.Drawing.Point(460, 39);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(110, 25);
             this.label9.TabIndex = 9;
@@ -136,7 +202,7 @@ namespace NeisByteCalculator
             this.label10.BackColor = System.Drawing.Color.Black;
             this.label10.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label10.ForeColor = System.Drawing.Color.White;
-            this.label10.Location = new System.Drawing.Point(476, 12);
+            this.label10.Location = new System.Drawing.Point(460, 12);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(110, 25);
             this.label10.TabIndex = 8;
@@ -146,7 +212,7 @@ namespace NeisByteCalculator
             // label7
             // 
             this.label7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label7.Location = new System.Drawing.Point(360, 42);
+            this.label7.Location = new System.Drawing.Point(348, 39);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(110, 25);
             this.label7.TabIndex = 7;
@@ -158,7 +224,7 @@ namespace NeisByteCalculator
             this.label8.BackColor = System.Drawing.Color.Black;
             this.label8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(360, 12);
+            this.label8.Location = new System.Drawing.Point(348, 12);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(110, 25);
             this.label8.TabIndex = 6;
@@ -168,7 +234,7 @@ namespace NeisByteCalculator
             // label5
             // 
             this.label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label5.Location = new System.Drawing.Point(244, 42);
+            this.label5.Location = new System.Drawing.Point(236, 39);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(110, 25);
             this.label5.TabIndex = 5;
@@ -180,7 +246,7 @@ namespace NeisByteCalculator
             this.label6.BackColor = System.Drawing.Color.Black;
             this.label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(244, 12);
+            this.label6.Location = new System.Drawing.Point(236, 12);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(110, 25);
             this.label6.TabIndex = 4;
@@ -190,7 +256,7 @@ namespace NeisByteCalculator
             // label3
             // 
             this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label3.Location = new System.Drawing.Point(128, 42);
+            this.label3.Location = new System.Drawing.Point(124, 39);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(110, 25);
             this.label3.TabIndex = 3;
@@ -202,7 +268,7 @@ namespace NeisByteCalculator
             this.label4.BackColor = System.Drawing.Color.Black;
             this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(128, 12);
+            this.label4.Location = new System.Drawing.Point(124, 12);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(110, 25);
             this.label4.TabIndex = 2;
@@ -212,7 +278,7 @@ namespace NeisByteCalculator
             // label2
             // 
             this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.label2.Location = new System.Drawing.Point(12, 42);
+            this.label2.Location = new System.Drawing.Point(12, 39);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(110, 25);
             this.label2.TabIndex = 1;
@@ -231,14 +297,23 @@ namespace NeisByteCalculator
             this.label1.Text = "항목";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // customRichTextBox1
+            // cbo_FontSize
             // 
-            this.customRichTextBox1.Location = new System.Drawing.Point(59, 51);
-            this.customRichTextBox1.Name = "customRichTextBox1";
-            this.customRichTextBox1.Size = new System.Drawing.Size(309, 163);
-            this.customRichTextBox1.TabIndex = 0;
-            this.customRichTextBox1.Text = "";
-            this.customRichTextBox1.TextChanged += new System.EventHandler(this.customRichTextBox1_TextChanged);
+            this.cbo_FontSize.Name = "cbo_FontSize";
+            this.cbo_FontSize.Size = new System.Drawing.Size(75, 39);
+            this.cbo_FontSize.TextUpdate += new System.EventHandler(this.cbo_FontSize_TextUpdate);
+            this.cbo_FontSize.TextChanged += new System.EventHandler(this.cbo_FontSize_TextChanged);
+            // 
+            // txt_Input
+            // 
+            this.txt_Input.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_Input.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txt_Input.Location = new System.Drawing.Point(0, 0);
+            this.txt_Input.Name = "txt_Input";
+            this.txt_Input.Size = new System.Drawing.Size(800, 300);
+            this.txt_Input.TabIndex = 0;
+            this.txt_Input.Text = "";
+            this.txt_Input.TextChanged += new System.EventHandler(this.txt_Input_TextChanged);
             // 
             // Frm_Main
             // 
@@ -248,8 +323,10 @@ namespace NeisByteCalculator
             this.Controls.Add(this.splitContainer1);
             this.Controls.Add(this.toolStrip1);
             this.Name = "Frm_Main";
-            this.Text = "Form1";
+            this.Text = "나이스(NEIS) 글자 수 계산기";
             this.Load += new System.EventHandler(this.Frm_Main_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
@@ -276,7 +353,14 @@ namespace NeisByteCalculator
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lbl_BytesInfo;
-        private CustomRichTextBox customRichTextBox1;
+        private CustomRichTextBox txt_Input;
+        private System.Windows.Forms.ToolStripButton cmd_Open;
+        private System.Windows.Forms.ToolStripButton cmd_Save;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripButton cmd_Exit;
+        private System.Windows.Forms.ToolStripComboBox cbo_FontName;
+        private System.Windows.Forms.ToolStripComboBox cbo_FontSize;
     }
 }
 
--- NeisByteCalculator/Frm_Main.cs
+++ NeisByteCalculator/Frm_Main.cs
@@ -17,10 +17,6 @@ namespace NeisByteCalculator
     public partial class Frm_Main : Form
     {
 
-        //private Thread thread;
-
-        //private CustomRichTextBox txt_Input;
-
         public Frm_Main()
         {
             InitializeComponent();
@@ -28,59 +24,27 @@ namespace NeisByteCalculator
 
         private void Frm_Main_Load(object sender, EventArgs e)
         {
-            /*
-                        txt_Input = new CustomRichTextBox();
-                        txt_Input.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-                        txt_Input.Location = new System.Drawing.Point(3, 24);
-                        txt_Input.Size = new System.Drawing.Size(785, 171);
-                        txt_Input.Text = "";
-                        txt_Input.TextChanged += new System.EventHandler(txt_Input_TextChanged);
+            // font list
+            foreach (FontFamily ff in FontFamily.Families)
+            {
+                cbo_FontName.Items.Add(ff.Name);
+                if (ff.Name.Equals(txt_Input.Font.Name))
+                    cbo_FontName.SelectedIndex = cbo_FontName.Items.Count - 1;
+            }
 
-                        this.splitContainer1.Panel1.Controls.Add(txt_Input);
-            */
-            /*
-
-            // 글자 수 계산을 Thread로 실행하기
-            thread = new Thread(CalculateThread);
-            thread.IsBackground = true;
-            thread.Start();
-
-            */
+            cbo_FontSize.Items.AddRange(new string[] {"8", "9", "10", "11", "12","14", "16", "18", "20","22","24","26","28","36","48","72" });
+            cbo_FontSize.Text = txt_Input.Font.Size.ToString();
 
             // 테스트를 위한 글자 입력 => 나이스 결과 : 공백 제외 90자, 공백 포함 99자, 157바이트
-            customRichTextBox1.Text = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+`~;:[]{}'/?\n동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리 나라 만세!";
-
+            txt_Input.Text = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()-_=+`~;:[]{}'/?\n동해물과 백두산이 마르고 닳도록\n하느님이 보우하사 우리 나라 만세!";
         }
-
-        private void CalculateThread()
-        {
-            try
-            {
-                while (true)
-                {
-                    Invoke(new Action(() =>
-                    {
-                        string input = customRichTextBox1.Text;
-                        int bytes = CalculateByteSize(input);
-                        int countChar = input.Length;
-                        int countCharWithoutSpace = input.Replace(" ", "").Replace("\n", "").Length;
-
-                        lbl_BytesInfo.Text = $"공백 제외 {countCharWithoutSpace}자, 공백 포함 {countChar}자, {bytes.ToString("#,##0")}바이트";
-                    }));
-
-                    Thread.Sleep(1);
-                }
-            }
-            catch (Exception)
-            {
-
-            }
-        }
-
 
 
         public static int CountCharacter(string input, char targetChar)
         {
+            // 문자 갯수를 세는 방법에 여러가지가 있지만, 이 방법이 가장 빠르다고 한다.
+            // https://stackoverflow.com/questions/541954/how-to-count-occurrences-of-a-char-string-within-a-string?rq=1
+            // 다른 방법으로는 Split을 사용하는 방법, Regular Expression을 사용하는 방법, IndexOf를 사용하는 방법, Count를 사용하는 방법 등 다양한 방법이 존재한다.
             int count = 0;
             foreach (char c in input)
             {
@@ -89,33 +53,129 @@ namespace NeisByteCalculator
             }
             return count;
         }
+
         public static int CalculateByteSize(string input)
         {
-            int bytes = Encoding.UTF8.GetBytes(input).Length;
-
-            return bytes + CountCharacter(input, '\r');
+            int bytes = Encoding.UTF8.GetBytes(input).Length;   // UTF8 Encoder를 사용하면 한글 1글자를 3Byte로 처리 함.
+            return bytes + CountCharacter(input, '\r');     // 엔터키가 한바이트로 처리(\r) 되므로 엔터키 갯수만큼 증가
+                                                            // RichTextBox에서는 Enter를 \n으로 인식했는데, CustomRichTextBox는 \r로 인식 함.
         }
 
 
         private void UpdateCharacterCounters()
         {
-            string input = customRichTextBox1.Text;
+            string input = txt_Input.Text;
             int bytes = CalculateByteSize(input);
             int countChar = input.Length;
-            int countCharWithoutSpace = input.Replace(" ", "").Replace("\r", "").Length;
+            int countCharWithoutSpace = input.Replace(" ", "").Replace("\r", "").Length;    // Space와 Enter 제거
 
             lbl_BytesInfo.Text = $"공백 제외 {countCharWithoutSpace}자, 공백 포함 {countChar}자, {bytes.ToString("#,##0")}바이트";
             Console.WriteLine($"공백 제외 {countCharWithoutSpace}자, 공백 포함 {countChar}자, {bytes.ToString("#,##0")}바이트");
         }
+
 
         private void txt_Input_TextChanged(object sender, EventArgs e)
         {
             UpdateCharacterCounters();
         }
 
-        private void customRichTextBox1_TextChanged(object sender, EventArgs e)
+        private void cmd_Open_Click(object sender, EventArgs e)
         {
-            UpdateCharacterCounters();
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Multiselect = false;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (!OpenFile(dialog.FileName, RichTextBoxStreamType.RichText))
+                {
+                    if (!OpenFile(dialog.FileName, RichTextBoxStreamType.PlainText))
+                    {
+                        MessageBox.Show(null, "파일 열기에 실패했습니다.", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+
+        private bool OpenFile(string fileName, RichTextBoxStreamType type)
+        {
+            try
+            {
+                txt_Input.LoadFile(fileName, type);
+                cbo_FontName.Text = txt_Input.Font.Name;
+                cbo_FontSize.Text = txt_Input.Font.Size.ToString();
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private void cmd_Save_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string fileName = dialog.FileName;
+                Console.WriteLine(System.IO.Path.GetExtension(fileName));
+                if (!System.IO.Path.GetExtension(fileName).Equals(".rtf", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (fileName.Substring(fileName.Length - 1, 1).Equals("."))
+                        fileName += "rtf";
+                    else
+                        fileName += ".rtf";
+                }
+
+                txt_Input.SaveFile(fileName, RichTextBoxStreamType.RichText);
+            }
+        }
+
+        private void cbo_FontName_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int selectIndex = txt_Input.SelectionStart;
+            int selectLength = txt_Input.SelectionLength;
+            try
+            {
+                Console.WriteLine("font size update");
+                txt_Input.SelectAll();
+                txt_Input.SelectionFont = new Font(cbo_FontName.Text, txt_Input.Font.Size);
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                txt_Input.SelectionStart = selectIndex;
+                txt_Input.SelectionLength = selectLength;
+            }
+        }
+
+        private void cbo_FontSize_TextUpdate(object sender, EventArgs e)
+        {
+        }
+
+        private void cbo_FontSize_TextChanged(object sender, EventArgs e)
+        {
+            int selectIndex = txt_Input.SelectionStart;
+            int selectLength = txt_Input.SelectionLength;
+            try
+            {
+                Console.WriteLine("font size update");
+                txt_Input.SelectAll();
+                txt_Input.SelectionFont = new Font(cbo_FontName.Text, float.Parse(cbo_FontSize.Text));
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                txt_Input.SelectionStart = selectIndex;
+                txt_Input.SelectionLength = selectLength;
+            }
         }
     }
 }
